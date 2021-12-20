31226124e8429bb1e9df80fd269593c08c7eb076
Franco Pizzi <fran.pizzi@hotmail.com>
2021-12-20 19:44:51 -0300

    Btn text landing

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -6,9 +6,7 @@ function Landing() {
   return (
     <div className={style.bk}>
        <Link to="/home/videogames" className={style.btn_pos}>
-            <button className={style.btn}>
-                ENTER 
-            </button>
+            <button className={style.btn}>ENTER</button>
         </Link>
     </div>
   );
